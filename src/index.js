fcd53f21151b92972a27da4ab03d3392391d19cf
ynkrpnk <ankarpenko35@gmail.com>
2026-05-07 23:58:17 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,70 @@
-"use strik";
-const arr = [1, 2, 3];
+'use strict';
 
-const myEvery = (arr, callback, thisArr) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (!callback.call(thisArr, arr[i], i, arr)) {
-      return false;
-    }
+function MyArray() {
+  this.length = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    this[i] = arguments[i];
+    this.length++;
   }
-  return true;
+}
+
+MyArray.isMyArray = function (obj) {
+  return obj instanceof MyArray;
 };
+
+function MyArrayProto() {
+  this.push = function () {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length++] = arguments[i];
+    }
+    return this.length;
+  };
+
+  this.forEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this);
+    }
+  };
+
+  this.concat = function (...args) {
+    const res = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+      res.push(this[i]);
+    }
+    for (let i = 0; i < args.length; i++) {
+      const currentArg = args[i];
+      if (MyArray.isMyArray(currentArg)) {
+        for (let j = 0; j < currentArg.length; j++) {
+          res.push(currentArg[j]);
+        }
+      } else {
+        res.push(currentArg);
+      }
+    }
+    return res;
+  };
+
+  this.flat = function (depth = 1) {
+    let result = new MyArray();
+
+    this.forEach((item) => {
+      if (MyArray.isMyArray(item) && depth > 0) {
+        const flattenedSubArray = item.flat(depth - 1);
+        result = result.concat(flattenedSubArray);
+      } else {
+        result = result.concat(item);
+      }
+    });
+
+    return result;
+  };
+}
+
+MyArray.prototype = new MyArrayProto();
+
+const myArr1 = new MyArray([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]]);
+const nested = new MyArray(myArr1); // [[1, 2], [3, 4], 5]
+
+
+const flatResult = nested.flat(1);
+console.log(flatResult);
